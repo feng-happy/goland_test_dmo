38fc4f2dadef241e5096f52c45741c06b0f234cb
Feng1216 <1306859182@qq.com>
2024-07-24 01:57:16 +0800

    测试添加一个按钮

---
+++ src/main/Java/com/example/demo1/HelloWorldAction.java
@@ -0,0 +1,17 @@
+package com.example.demo1;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author CZF
+ * @create 2024-07-16 23:16
+ */
+public class HelloWorldAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+
+
+    }
+}
